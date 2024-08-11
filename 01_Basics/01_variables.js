795a406398299cb161330820504311e1fff4b4d6
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-11 23:50:34 +0530

    Update 01_variables.js

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -6,3 +6,13 @@ accountCity = "Sagar"
 // accountId = 67565454 // : Not allowed because constant
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
+// output
+// ┌─────────┬────────────────┐
+// │ (index) │ Values         │
+// ├─────────┼────────────────┤
+// │ 0       │ 7870954        │
+// │ 1       │ 'sahil@MS.com' │
+// │ 2       │ '12345'        │
+// │ 3       │ 'Sagar'        │
+// └─────────┴────────────────┘
+
