839107edb4e5545d348f15ed1641115f4feb34e4
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-12 18:23:12 +0000

    type of comparisons

--- 01_Basics/03_conversationOpration.js
+++ 01_Basics/03_conversationOpration.js
@@ -3,3 +3,9 @@ let score = "33"
 
 let ValueInNumber = Number(score)
 // console.log(ValueInNumber); // NaN (Not a Number) (but type Number)
+
+// number conversion
+// "33" => string
+// "33abc" => NaN
+// true => 1 / false => 0
+// {} => NaN
