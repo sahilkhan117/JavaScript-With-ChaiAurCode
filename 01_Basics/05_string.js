bda6918d317495ca9ef351cea47ae245a194e4dd
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-17 11:48:21 +0000

    a bad practice

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -3,4 +3,4 @@
 
 // const name = "sahil";
 // const repoCount = 5;
-console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); //🙅🏻‍♂️⚠️ Bad practice not use in Industry
