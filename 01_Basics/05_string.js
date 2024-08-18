a749c67019b0fa832cdb797f6814b27f7ef43c4e
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-18 04:16:16 +0000

    str, ibg

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -48,4 +48,5 @@ length: 5
 */
 
 console.log(`1. anchor("www.sahil.com") : ${gameName.anchor("www.sahil.com")} => make a link with a # tag`); 
+console.log(`2. big() : ${gameName.big()} => make text big`);
 
