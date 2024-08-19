25fd78275bd55bfc91454b31b636f7d32e7bf5f0
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-19 04:52:56 +0000

    str . substr

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -68,6 +68,7 @@ console.log(`18. small() : ${gameName.small()} => make text small`);
 console.log(`20. split("_") : ${gameName.split("_")} => split string with _`);
 console.log(`21. strike() : ${gameName.strike()} => make text strike`);
 console.log(`22. sub() : ${gameName.sub()} => make text sub`);
+console.log(`23. substr() : ${gameName.substr()} => make text substr`);
 
 
 
