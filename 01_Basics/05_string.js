d598de74e8d454750041172a9f92239ede6dade8
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-19 04:54:58 +0000

    str .tostring

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -74,6 +74,7 @@ console.log(`25. sup() : ${gameName.sup()} => make text sup`);
 console.log(`26. toLocaleLowerCase() : ${gameName.toLocaleLowerCase()} => make text toLocaleLowerCase`);
 console.log(`27. toLocaleUpperCase() : ${gameName.toLocaleUpperCase()} => make text toLocaleUpperCase`);
 console.log(`28. toLowerCase() : ${gameName.toLowerCase()} => make text toLowerCase`);
+console.log(`29. toString() : ${gameName.toString()} => make text toString`);
 
 
 
