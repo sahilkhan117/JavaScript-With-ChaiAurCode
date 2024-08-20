9a75a25d9cad0dab1a3323f3f6f56777fcbcfd4c
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-20 05:06:48 +0000

    some console

--- 01_Basics/06_nums_math.js
+++ 01_Basics/06_nums_math.js
@@ -4,3 +4,13 @@ const score = 400;
 const balance = new Number(875);
 // console.log(balance);
 /*
+Number {100}
+[[Prototype]]: Number
+    constructor: ƒ Number()
+    toExponential: ƒ toExponential()
+    toFixed: ƒ toFixed()
+    toLocaleString: ƒ toLocaleString()
+    toPrecision: ƒ toPrecision()
+    toString: ƒ toString()
+    valueOf: ƒ valueOf()
+*/
