1122c45b33e8385951433b04d7d5a9479fc6a046
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-22 03:00:14 +0000

    remove comments

--- 01_Basics/07_datetime.js
+++ 01_Basics/07_datetime.js
@@ -2,5 +2,5 @@
 
 let myDate = new Date();
 
-// console.log(myDate); // 2024-08-16T17:52:13.595Z (obj)
-// console.log(myDate.toString()); // Fri Aug 16 2024 23:30:48 GMT+0530 (India Standard Time)
+console.log(myDate); // 2024-08-16T17:52:13.595Z (obj)
+console.log(myDate.toString()); // Fri Aug 16 2024 23:30:48 GMT+0530 (India Standard Time)
