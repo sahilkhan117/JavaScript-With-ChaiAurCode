7795a5ed7df4dcd63e44a765fc5f9fef472c955f
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-22 03:00:52 +0000

    to time string

--- 01_Basics/07_datetime.js
+++ 01_Basics/07_datetime.js
@@ -6,3 +6,4 @@ console.log(myDate); // 2024-08-16T17:52:13.595Z (obj)
 console.log(myDate.toString()); // Fri Aug 16 2024 23:30:48 GMT+0530 (India Standard Time)
 
 console.log(myDate.toDateString()); // Fri Aug 16 2024
+console.log(myDate.toTimeString()); // 23:33:43 GMT+0530 (India Standard Time)
