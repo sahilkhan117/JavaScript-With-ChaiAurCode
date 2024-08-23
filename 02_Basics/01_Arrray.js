d4405c6867148f4f480d31411b0d09b8656ba259
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-23 04:14:52 +0000

    assign a array

--- 02_Basics/01_Arrray.js
+++ 02_Basics/01_Arrray.js
@@ -3,3 +3,6 @@
 // Array is a Non-Primitive (Object DataType)
 // Array are resizable
 // Array copy Opration create Shallow Copy ()
+
+const myArr = [10, "sahil", true, 1, 2, 3]
+// console.log(myArr[1]);
