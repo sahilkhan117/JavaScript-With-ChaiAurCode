8e6e4504b4329fee71b6ebb2cf11d6e506a51dca
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-23 04:24:57 +0000

    test all methods

--- 02_Basics/01_Arrray.js
+++ 02_Basics/01_Arrray.js
@@ -82,3 +82,47 @@ Symbol(Symbol.iterator): ƒ values()
 Symbol(Symbol.unscopables): {a: rue, copyWithi: rue, entrie: rue, fil: rue, fin: rue, …}
 */
 
+
+
+
+console.log(`1. at() => ${myArr.at(2)} : returns element of index`);
+console.log(`2. concat() => ${myArr.concat([1,2,3,4,5])} : concatenate arrey`);
+console.log(`3. constructor() => ${myArr.constructor} : returns constructor of array`);
+console.log(`4. copyWithin() => ${myArr.copyWithin(0, 1, 2)} : copy within`);
+console.log(`5. entries() => ${myArr.entries()} : return iterator of array`);
+console.log(`6. every() => ${myArr.every} : return true if every element is true`);
+console.log(`7. fill() => ${myArr.fill(2)} : fill array with value`);
+// console.log(`8. filter() => ${myArr.filter} : return array after filter`);
+// console.log(`9. find() => ${myArr.find()} : return first element`);
+// console.log(`10. findIndex() => ${myArr.findIndex()} : return index of first element`);
+// console.log(`11. findLast() => ${myArr.findLast()} : return last element`);
+// console.log(`12. findLastIndex() => ${myArr.findLastIndex()} : return index of last element`);
+console.log(`13. flat() => ${myArr.flat()} : return array after flat`);
+// console.log(`14. flatMap() => ${myArr.flatMap()} : return array after flatMap`);
+// console.log(`15. forEach() => ${myArr.forEach()} : return array after forEach`);
+console.log(`16. includes() => ${myArr.includes()} : return true if element is present`);
+console.log(`17. indexOf() => ${myArr.indexOf()} : return index of element`);
+console.log(`18. join() => ${myArr.join()} : return string after join`);
+console.log(`19. keys() => ${myArr.keys()} : return iterator of keys`);
+console.log(`20. lastIndexOf() => ${myArr.lastIndexOf()} : return index of last element`);
+console.log(`21. length => ${myArr.length} : return length of array`);
+// console.log(`22. map() => ${myArr.map()} : return array after map`);
+console.log(`23. pop() => ${myArr.pop()} : return last element`);
+console.log(`24. push() => ${myArr.push()} : return length of array`);
+// console.log(`25. reduce() => ${myArr.reduce()} : return array after reduce`);
+// console.log(`26. reduceRight() => ${myArr.reduceRight()} : return array after reduceRight`);
+console.log(`27. reverse() => ${myArr.reverse()} : return array after reverse`);
+console.log(`28. shift() => ${myArr.shift()} : return first element`);
+console.log(`29. slice() => ${myArr.slice()} : return array after slice`);
+// console.log(`30. some() => ${myArr.some()} : return true if some element is true`);
+console.log(`31. sort() => ${myArr.sort()} : return array after sort`);
+console.log(`32. splice() => ${myArr.splice()} : return array after splice`);
+console.log(`33. toLocaleString() => ${myArr.toLocaleString()} : return array after toLocaleString`);
+console.log(`34. toReversed() => ${myArr.toReversed()} : return array after toReversed`);
+console.log(`35. toSorted() => ${myArr.toSorted()} : return array after toSorted`);
+console.log(`36. toSpliced() => ${myArr.toSpliced()} : return array after toSpliced`);
+console.log(`37. toString() => ${myArr.toString()} : return array after toString`);
+console.log(`38. unshift() => ${myArr.unshift()} : return length of array`);
+console.log(`39. values() => ${myArr.values()} : return iterator of values`);
+console.log(`40. with() => ${myArr.with()} : return array after with`);
+
