252016d694dd9dd6a408e0aa0eb90685661feff0
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-24 02:51:14 +0000

    concat method margeit

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -7,3 +7,4 @@ const DCU_heros = ["SuperMan", "Batman", "Flash"]
 // MCU_heros.push(DCU_heros) // [ 'Iron-Man', 'Spider-Man', 'Thor', [ 'SuperMan', 'Batman', 'Flash' ] ]
 // console.log(MCU_heros[3][1]); // batman
 
+// let all_heros = MCU_heros.concat(DCU_heros) // [ 'Iron-Man', 'Spider-Man', 'Thor', 'SuperMan', 'Batman', 'Flash' ] 
