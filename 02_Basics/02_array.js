abe4a9f1823378d1b3d6f80766af1d3025892199
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-24 02:50:17 +0000

    push is fail

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -2,3 +2,6 @@ const MCU_heros = ["Iron-Man", "Spider-Man", "Thor"]
 const DCU_heros = ["SuperMan", "Batman", "Flash"]
 
 // merge them with push 🆚 concat 🆚  🤩speread(efficient)
+
+// ****** ❌ PUSH 
+// MCU_heros.push(DCU_heros) // [ 'Iron-Man', 'Spider-Man', 'Thor', [ 'SuperMan', 'Batman', 'Flash' ] ]
