ffe8988a6154ed1b6dec83e9f7e2c70955990451
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-26 04:58:58 +0000

    some other methods

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -52,3 +52,21 @@ console.log(Object.entries(tinUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sah' ]
 
 // validating / check available property
 console.log(tinUser.hasOwnProperty('isLaggedIn')); // true
+
+/* ⭐⭐⭐ METHODS 
+[[Prototype]]: Object
+    constructor: ƒ Object()
+    hasOwnProperty: ƒ hasOwnProperty() 
+    isPrototypeOf: ƒ isPrototypeOf()   
+    propertyIsEnumerable: ƒ propertyIsEnumerable() 
+    toLocaleString: ƒ toLocaleString() 
+    toString: ƒ toString() 
+    valueOf: ƒ valueOf()   
+    __defineGetter__: ƒ __defineGetter__() 
+    __defineSetter__: ƒ __defineSetter__() 
+    __lookupGetter__: ƒ __lookupGetter__() 
+    __lookupSetter__: ƒ __lookupSetter__() 
+    __proto__: (...)   
+    get __proto__: ƒ __proto__()   
+    set __proto__: ƒ __proto__()   
+*/
