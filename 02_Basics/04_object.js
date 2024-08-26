71db9b97cc92dbbe78309790da1a3d3a2d8a3d89
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-26 04:52:47 +0000

    declare as nested object

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -5,3 +5,13 @@ const tinUser = {} // empty obj
 tinUser.id = "123abc"
 tinUser.name = "Sah"
 tinUser.isLaggedIn = false
+
+const regularUser  = {
+    emial : "some@gmail.com",
+    fullname : { // ⛓️nested 
+        userFullName : { // ⛓️nested 
+            firstName : "sahil",
+            lastName : "khan",
+        }
+    }
+}
