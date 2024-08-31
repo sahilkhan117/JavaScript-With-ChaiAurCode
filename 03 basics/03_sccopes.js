c647be41fd489d87547927e443ceca9a1dc8e199
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-08-31 02:55:34 +0000

    make other inners

--- 03 basics/03_sccopes.js
+++ 03 basics/03_sccopes.js
@@ -1,13 +1,17 @@
 // {} => used as scope with any if, for, func ,etc...
 
-let a = 10 // 👍🏻 in the scope only
-const b  = 20 // constant in scope only
-var c = 30 //⚠️ 💥💥scope - biscope se koi matlab nahi
+let a = 10; // 👍🏻 in the scope only
+const b = 20; // constant in scope only
+var c = 30; //⚠️ 💥💥scope - biscope se koi matlab nahi
 
 if (true) {
-    let a = 100
-    const b = 200
-    var c = 300
+	let a = 100;
+	const b = 200;
+	var c = 300;
 
-    console.log("INNER a :", a);
+	console.log("INNER a :", a);
+	console.log("INNER a :", b);
+	console.log("INNER a :", c);
 }
+
+console.log("a", a); // diff as inner (😎😎🤩best to use)
