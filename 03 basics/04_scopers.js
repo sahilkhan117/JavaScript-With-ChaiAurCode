9161574cb7f23a5b2d6624649ce921cbc4298df0
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-02 11:59:18 +0000

    call two func

--- 03 basics/04_scopers.js
+++ 03 basics/04_scopers.js
@@ -1,12 +1,16 @@
 // USE Ice - Cream 🍧🍦 example
-// Bachche 👦🏻 bado 🧔🏻‍♂️ se 🍧 mang sakte hai par bade🧔🏻‍♂️ bachcho 👦🏻 se 🍦 nahi mnag sakte❌ 
+// Bachche 👦🏻 bado 🧔🏻‍♂️ se 🍧 mang sakte hai par bade🧔🏻‍♂️ bachcho 👦🏻 se 🍦 nahi mnag sakte❌
 
 // IN NESTED FUNC
-function one (){
-    const username = "sahil"
+function one() {
+	const username = "sahil";
 
-    function two(){
-        const website = "YT"
+	function two() {
+		const website = "YT";
 
-        console.log(username); // ✅ access
-    }
+		console.log(username); // ✅ access
+	}
+	// console.log(website); // ❌ out of scope
+
+	two();
+}
