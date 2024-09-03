e88f9879098e692952e102f169bb2d47fc8b37c1
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-03 17:09:49 +0000

    this in a function

--- 03 basics/05_arrow_func.js
+++ 03 basics/05_arrow_func.js
@@ -18,3 +18,31 @@ user.username = "Sahil";
 // GLOBAL THIS
 // console.log(this); // {}
 // 🔥🧐 BUT in web there have a Window object Contains document and inner structures 
+
+function chai() {
+	console.log(this);
+	/*
+    <ref *1> Object [global] {
+        global: [Circular *1],
+        clearImmediate: [Function: clearImmediate],
+        setImmediate: [Function: setImmediate] {
+          [Symbol(nodejs.util.promisify.custom)]: [Getter]
+        },
+        clearInterval: [Function: clearInterval],
+        clearTimeout: [Function: clearTimeout],
+        setInterval: [Function: setInterval],
+        setTimeout: [Function: setTimeout] {
+            [Symbol(nodejs.util.promisify.custom)]: [Getter]
+        },
+        queueMicrotask: [Function: queueMicrotask],
+        structuredClone: [Function: structuredClone],
+        atob: [Getter/Setter],
+        btoa: [Getter/Setter],
+        performance: [Getter/Setter],
+        fetch: [Getter/Setter],
+        navigator: [Getter],
+        crypto: [Getter]
+    }
+  */
+}
+// chai();
