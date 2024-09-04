c448aa7bfe1859bb6f9ebcd1b0f103faac85a1b1
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-04 07:41:00 +0000

    why IIFE

--- 03 basics/06_IIFE.js
+++ 03 basics/06_IIFE.js
@@ -1,3 +1,7 @@
 // Immidiately Invoked Function Expression (IIFE)
 
 // jaise hi fn llikhaturant hi execute karvana
+
+// ❓❓❓❓ WHY ❓❓❓❓
+// 1️⃣ Remove 😶‍🌫️Pullution of Global scope variable (accesable for childs🍧 )
+// 2️⃣ Immideately Execute 🏃🏻‍➡️🏃🏻‍♀️‍➡️🏃🏻‍♂️‍➡️
