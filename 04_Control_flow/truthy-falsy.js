8d975d288b41ad419ddce4be1ee90b44bb9bf182
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-13 08:45:57 +0000

    list of falsy values

--- 04_Control_flow/truthy-falsy.js
+++ 04_Control_flow/truthy-falsy.js
@@ -9,3 +9,6 @@ else {
 // Got User (means epty arr is true)
 
 // ⭐⭐⭐⭐ some type of values refer always truth or false used in condition
+
+// 🤥🤥 FALSY : (only some)
+// false, 0, -0, BigInt 0n, "", null, undefied, NaN
