9ea383cef0d4dfcee2973cc1038a54f7728e1e2b
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-17 03:06:17 +0000

    print table of 1 to 10

--- 05_loops/for.js
+++ 05_loops/for.js
@@ -5,3 +5,13 @@
 for (let i = 0; i < 10; i++) {
     // console.log(i);
 }
+
+// ⭐⭐ print 1 to 10 table
+y = []
+for (let i = 1; i <= 10; i++) {
+    x = []
+    for (let j = 1; j <= 10; j++) {
+        x[j] = `${i}*${j} = ${i*j}`
+    }
+    y[i] = x
+}
