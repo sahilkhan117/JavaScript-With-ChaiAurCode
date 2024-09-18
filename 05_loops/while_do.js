a8c2ce8d8b239f5b549a5c280d76a96b539708ab
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-18 12:17:00 +0000

    then the updation

--- 05_loops/while_do.js
+++ 05_loops/while_do.js
@@ -1,6 +1,9 @@
 // while : ittrate over the condition (entry control loop)
 
-i = 1 // declaration
-while (i < 10) { // condition check
-    // if true so execute
-    console.log(i);
+i = 1; // declaration
+while (i < 10) {
+	// condition check
+	// if true so execute
+	console.log(i);
+	i++; // increment
+}
