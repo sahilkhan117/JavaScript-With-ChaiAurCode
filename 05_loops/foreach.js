317714b955d3a6c6d3e303bfa1fc6c310d10abf1
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-27 17:24:48 +0000

    simple use

--- 05_loops/foreach.js
+++ 05_loops/foreach.js
@@ -5,3 +5,7 @@ const coding = ["js", "ruby", "python", "java", "cpp", "html"]
 // coding.forEach( function (item) {
 //     < block >
 // })
+
+coding.forEach( function (item, index, arr) {
+    // console.log(item, index, arr) // js 0 [ 'js', 'ruby', 'python', 'java', 'cpp', 'html' ]
+})
