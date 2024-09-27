a7c877c43a0158bb1df1a5b797ffaa0db7f15ce6
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-27 17:25:55 +0000

    Introduction to arrow function

--- 05_loops/foreach.js
+++ 05_loops/foreach.js
@@ -8,7 +8,11 @@ const coding = ["js", "ruby", "python", "java", "cpp", "html"]
 
 coding.forEach( function (item, index, arr) {
     // console.log(item, index, arr)
-    
+
     // OUTPUT
     // js 0 [ 'js', 'ruby', 'python', 'java', 'cpp', 'html' ]
 })
+
+
+// ARROE FUNC
+// coding.forEach( () => {} )
