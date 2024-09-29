4cb08cf4cbf0e5513844d33372bfcd30d4670cef
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-29 04:54:26 +0000

    peint filtered array

--- 05_loops/filter.js
+++ 05_loops/filter.js
@@ -10,3 +10,5 @@ const coding = ["js", "ruby", "python", "java", "cpp", "html"]
 
 let mynums = [1,2,3,4,5,6,7,8,9,10]
 const newNums = mynums.filter( (i) => i>4) // if true they add to return array
+
+console.log(newNums); // [ 5, 6, 7, 8, 9, 10 ]
