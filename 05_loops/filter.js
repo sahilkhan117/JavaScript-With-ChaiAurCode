95670a9a153d35c1c086efb61d1c3d62d3d04909
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-09-29 04:52:37 +0000

    the foreach cant return anythig

--- 05_loops/filter.js
+++ 05_loops/filter.js
@@ -1,1 +1,6 @@
 const coding = ["js", "ruby", "python", "java", "cpp", "html"]
+
+// coding.forEach( (i, idx) => (console.log(`${idx+1}. ${i}`)))
+// let newar = coding.forEach( (i, idx) => (`${idx+1}. ${i}`))
+// console.log(newar); // 😵‍💫😵‍💫 undefined (foreack cant return anything)
+
