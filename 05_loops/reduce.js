1fcd7efe8a7345aa5da969e01353e40e21eae704
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-10-06 04:40:10 +0000

    apply the reduce on num

--- 05_loops/reduce.js
+++ 05_loops/reduce.js
@@ -2,3 +2,4 @@
 // sum = arr.reduce( (acc, i) => (new acc value), < initial acc value>) // return final acc val
 
 let num = [1,2,3,4]
+let sum = num.reduce( (acc, i) => (acc + i), 0) // sum of elem of arr
