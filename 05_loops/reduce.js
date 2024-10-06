22256555531bacfea7dad877ef64904c66131ae0
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-10-06 04:41:25 +0000

    make a api structure

--- 05_loops/reduce.js
+++ 05_loops/reduce.js
@@ -5,3 +5,10 @@ let num = [1,2,3,4]
 let sum = num.reduce( (acc, i) => (acc + i), 0) // sum of elem of arr
 console.log(sum);
 // 10
+
+let curses = [
+    { name: "Js cource", price: 2999},
+    { name: "py cource", price: 999},
+    { name: "Mobile Dev cource", price: 5999},
+    { name: "Data Science cource", price: 12999},
+]
