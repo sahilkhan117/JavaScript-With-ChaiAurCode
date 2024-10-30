959b9caed00c65564821140655210552166714e5
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-10-30 04:25:47 +0000

    select the result and msg boxes

--- 07_simple_Projects/02_BMI/script.js
+++ 07_simple_Projects/02_BMI/script.js
@@ -2,3 +2,6 @@ const form = document.querySelector("form");
 
 const h = document.querySelector("#height");
 const w = document.querySelector("#weight");
+
+const result = document.querySelector("#result");
+const msg = result.nextElementSibling;
