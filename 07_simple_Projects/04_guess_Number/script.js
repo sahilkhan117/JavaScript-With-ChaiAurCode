2527109966e0276ffbf233a2803c7460b7c45a0e
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-05 03:14:48 +0000

    select some htmlElements

--- 07_simple_Projects/04_guess_Number/script.js
+++ 07_simple_Projects/04_guess_Number/script.js
@@ -1,4 +1,9 @@
 let randomNumber = Math.ceil(Math.random() * 100);
 // console.log(randomNumber);
 
-
+const userInput = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const GuessSlot = document.querySelector("#guessSlot");
+const remaining = document.querySelector("#attempts");
+const lowOrHi = document.querySelector("#lowOrHi");
+const startOver = document.querySelector(".resultpara");
