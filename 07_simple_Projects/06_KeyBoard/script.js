95912e2381606e32a24a363427afb991413af4a6
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-11 17:03:52 +0000

    add a keyDown event listner

--- 07_simple_Projects/06_KeyBoard/script.js
+++ 07_simple_Projects/06_KeyBoard/script.js
@@ -1,10 +1,43 @@
-let i = document.querySelector("#in")
-let color = randomColor()
+let i = document.querySelector("#in");
+let color = randomColor();
 
 function randomColor() {
-    let hex = "456789ABCDEF"
-    let c = "#"
-    for (let i = 0; i < 6; i++) c += hex[Math.floor(Math.random() * 12)]
+	let hex = "456789ABCDEF";
+	let c = "#";
+	for (let i = 0; i < 6; i++) c += hex[Math.floor(Math.random() * 12)];
 
-    return c
+	return c;
 }
+
+document.addEventListener("keydown", (e) => {
+	let span = document.createElement("span");
+	span.style.color = color;
+
+	if (e.keyCode == 8) {
+		// Backspace
+		i.lastElementChild.remove();
+	} else if (e.keyCode == 13) {
+		// Enter
+		span.innerHTML = "<br>";
+		i.appendChild(span);
+	} else if (
+		e.keyCode == 16 ||
+		e.keyCode == 17 ||
+		e.keyCode == 18 ||
+		e.keyCode == 20
+	) {
+		// Shift, ctrl, alt, caps Lock
+	} else if (e.keyCode >= 37 && e.keyCode <= 40) {
+		// F keys
+	} else if (e.keyCode > 112 && e.keyCode < 124) {
+		// F keys
+	} else {
+		// All Other Characters
+		span.innerText = e.key;
+		i.appendChild(span);
+	}
+
+	// console.log(e.keyCode);
+
+	if (e.key == " " || e.keyCode == 13) color = randomColor(); // space or Enter
+});
