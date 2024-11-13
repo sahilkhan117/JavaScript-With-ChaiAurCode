b0db27733441d6c8fae8a9b9d1707ee072bf2e12
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-13 18:42:15 +0000

    done the js file

--- 10_Advanced-1/ApiRequest.js
+++ 10_Advanced-1/ApiRequest.js
@@ -5,4 +5,10 @@ xhr.open("GET", url);
 xhr.onreadystatechange = () => {
     console.log(xhr.readyState);
 
+    if(xhr.readyState == 4){
+        let data = JSON.parse(this.responceText)
+        console.log(data.followers);
+    }
 };
+
+xhr.send()
