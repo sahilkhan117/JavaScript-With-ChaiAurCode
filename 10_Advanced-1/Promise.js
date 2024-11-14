4cd5ee3e44ae0e54f822c7149c2c836539ccf532
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-14 04:28:25 +0000

    make a then part

--- 10_Advanced-1/Promise.js
+++ 10_Advanced-1/Promise.js
@@ -2,11 +2,15 @@
 // the promice object represents the eventual completion (or failure) of an asuncronous opration and its resulting value
 
 const promiceOne = new Promise(function (resolve, reject) {
-    
+
     // Do any async task
 	// Network Call, DB call, Cryptography
 	setTimeout(function () {
 		console.log("Async Task Is complete");
 		resolve(); // connect resolve 🔗 then
 	}, 1000);
+});
+
+promiceOne.then(function () {
+	console.log("Promise consumed");
 });
