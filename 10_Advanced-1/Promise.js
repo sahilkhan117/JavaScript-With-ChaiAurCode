cd9fb83d7ecd0eae52510014b905c89731b4cf90
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-14 04:24:14 +0000

    make a promice object

--- 10_Advanced-1/Promise.js
+++ 10_Advanced-1/Promise.js
@@ -1,2 +1,5 @@
 // PROMISE
 // the promice object represents the eventual completion (or failure) of an asuncronous opration and its resulting value
+
+const promiceOne = new Promise(function (resolve, reject) {
+});
