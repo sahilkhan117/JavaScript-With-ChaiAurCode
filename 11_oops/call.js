2171b365df5e26efad9b37201916ff792cd644ca
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-17 15:06:48 +0000

    how to call with new keyword

---
+++ 11_oops/call.js
@@ -0,0 +1,1 @@
+// Jab class ya (new function 😁) me koi or function call karte hai jo uski varibles ko read ya write harta hai to use perticularly batana padhta hai ki kis scope ke variables like ⭐'This' ko use karo.
