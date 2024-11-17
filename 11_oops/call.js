4cca8693d15a259d19fc44407a36290506d5be0e
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-17 15:07:15 +0000

    use call()

--- 11_oops/call.js
+++ 11_oops/call.js
@@ -1,1 +1,10 @@
 // Jab class ya (new function 😁) me koi or function call karte hai jo uski varibles ko read ya write harta hai to use perticularly batana padhta hai ki kis scope ke variables like ⭐'This' ko use karo.
+
+// Isi kam ke liye 'call()' Method ka use karte hai
+// EXAMPLE 👇🏻👇🏻👇🏻
+
+function setUserName(username){
+    // Complex DB Call
+    this.username = username
+    console.log("Called");
+}
