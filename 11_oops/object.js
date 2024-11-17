36c6aa27dedf33c47e43a7298909ef4dceed1120
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-17 15:05:26 +0000

    test fn

--- 11_oops/object.js
+++ 11_oops/object.js
@@ -1,1 +1,11 @@
 // 😁🧐 Har cheej ek object hai (see oop.md)
+
+function multiplyBy5(num){
+    return num*5
+}
+multiplyBy5.power = 2 // 😁 bas ban gaya
+
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype); // {}
+
