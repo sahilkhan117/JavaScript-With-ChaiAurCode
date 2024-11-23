e6ba2c973420ba013aba0a6eef9e57c168feb5d2
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-23 13:43:37 +0000

    my desired o/p

--- 11_oops/prototype.js
+++ 11_oops/prototype.js
@@ -1,3 +1,6 @@
 let myname = "   hitesh       ";
 
 // TODO: Make a Custom method that Remove extra spaces from the string start and end and return the the true lenght of the string.
+
+// ⚡ My desired output 
+// console.log( myname.trueLength()); 
