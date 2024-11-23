9add011ae7d5256b92c25cb4b3fb5ac0d7c4cc90
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-23 13:44:15 +0000

    some case

--- 11_oops/prototype.js
+++ 11_oops/prototype.js
@@ -2,5 +2,12 @@ let myname = "   hitesh       ";
 
 // TODO: Make a Custom method that Remove extra spaces from the string start and end and return the the true lenght of the string.
 
-// ⚡ My desired output 
+//⚡ My desired output
 // console.log( myname.trueLength()); 
+
+// 🆗 but 🧐 can i make this without any function
+// String.prototype.trueLength = function(){
+//     return `True Lenght of ${this} is ${this.trim().length}`
+// }
+
+// console.log(myname.trueLength);
