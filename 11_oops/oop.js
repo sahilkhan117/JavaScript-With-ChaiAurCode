fb62233b56c95a811bc4cda72703355bd73f2a89
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-24 04:17:28 +0000

    but the safe way is to use new keyword

--- 11_oops/oop.js
+++ 11_oops/oop.js
@@ -1,25 +1,28 @@
 const user = {
-    username: "sahil",
-    loginCount: 8,
-    isLoggedIn: true,
-    greet: function () {
-        console.log("hello", this.username);
-    }
-}
+	username: "sahil",
+	loginCount: 8,
+	isLoggedIn: true,
+	greet: function () {
+		console.log("hello", this.username);
+	},
+};
 
 console.log(user);
 console.log(user.greet());
 
 function User(username, loginCount, isLoggedIn) {
-    this.username = username;
-    this.loginCount = loginCount;
-    this.isLoggedIn = isLoggedIn;
+	this.username = username;
+	this.loginCount = loginCount;
+	this.isLoggedIn = isLoggedIn;
 
-    this.greet = function () {
-        console.log("hello", this.username);
-    }
+	this.greet = function () {
+		console.log("hello", this.username);
+	};
 
-    return this
+	return this;
 }
 
-const UserOne = User("sahil", 8, true); // if 🆕 not use this changed 
+// const UserOne = User("sahil", 8, true); // if 🆕 not use this changed
+
+const UserOne = new User("sahil", 8, true);
+const UserTwo = new User("chai", 4, false);
