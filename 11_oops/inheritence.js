36cec312bf048d1ca02c14095f63c6076b5b3849
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-27 13:48:36 +0000

    write about class

--- 11_oops/inheritence.js
+++ 11_oops/inheritence.js
@@ -1,18 +1,26 @@
 // Inheritence: Inheritence is a mechanism in which a new class is created from an existing class. The new class is called the derived class and the existing class is called the base class. The derived class inherits all the features from the base class and can have additional features of its own.
 
-
 // Make a top parent level class
-class User{
-    constructor(username){
-        this.username = username
-    }
+class User {
+	constructor(username) {
+		this.username = username;
+	}
 
-    logMe(){
-        console.log(`Log Me ${this.username}`);
-    }
+	logMe() {
+		console.log(`Log Me ${this.username}`);
+	}
 }
 
 // Make a child class of User
-class Teacher extends User{
-    // code here
+class Teacher extends User {
+	constructor(username, email, password) {
+		
+
+		this.email = email;
+		this.password = password;
+	}
+
+	addCourse() {
+		console.log(`A new Course added by ${this.username}`);
+	}
 }
