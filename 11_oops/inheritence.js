452f3a57f5bfd8c291978819b06644517cbaf4c8
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-27 13:46:53 +0000

    top parent level class

--- 11_oops/inheritence.js
+++ 11_oops/inheritence.js
@@ -1,2 +1,13 @@
 // Inheritence: Inheritence is a mechanism in which a new class is created from an existing class. The new class is called the derived class and the existing class is called the base class. The derived class inherits all the features from the base class and can have additional features of its own.
 
+
+// Make a top parent level class
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Log Me ${this.username}`);
+    }
+}
