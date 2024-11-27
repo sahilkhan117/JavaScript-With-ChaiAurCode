37a343b6828db77f0f2bbe3c11d3bc92f5fccd87
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-27 13:45:51 +0000

    learn about Inheritence

---
+++ 11_oops/inheritence.js
@@ -0,0 +1,2 @@
+// Inheritence: Inheritence is a mechanism in which a new class is created from an existing class. The new class is called the derived class and the existing class is called the base class. The derived class inherits all the features from the base class and can have additional features of its own.
+
