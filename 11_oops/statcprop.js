39dd6206cb3ab91720d0250a0b93d3f6420d5431
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-28 08:23:34 +0000

    make a test class

--- 11_oops/statcprop.js
+++ 11_oops/statcprop.js
@@ -3,3 +3,13 @@
 // Static methods: Static methods are called on the class itself, not on instances of the class. They are often used to create utility functions for an application.
 
 // Static properties and methods are defined on the class itself. You can access them without instantiating the class. They are often used to create utility functions for an application.
+
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+}
