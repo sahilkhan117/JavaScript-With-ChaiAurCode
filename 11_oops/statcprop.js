83c2cea7018b535f16b911d257cf53c2170aeddc
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-11-28 08:23:21 +0000

    static method

---
+++ 11_oops/statcprop.js
@@ -0,0 +1,5 @@
+// Static Methods and Properties
+// Static properties: Static properties are shared among all instances of a class, while instance properties are unique to each instance of a class.
+// Static methods: Static methods are called on the class itself, not on instances of the class. They are often used to create utility functions for an application.
+
+// Static properties and methods are defined on the class itself. You can access them without instantiating the class. They are often used to create utility functions for an application.
