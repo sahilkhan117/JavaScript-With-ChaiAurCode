264facfd347435e83b34d791843a1dc4b0edf214
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-01 11:24:02 +0000

    its info

--- 11_oops/mathpi.js
+++ 11_oops/mathpi.js
@@ -2,3 +2,11 @@ const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
 console.log(descripter);
 
 // getOwnPropertyDescriptor: this function returns the property descriptor of the specified property of the given object.
+
+// 😵‍💫
+// {
+//   value: 3.141592653589793,
+//   writable: false,
+//   enumerable: false,
+//   configurable: false
+// }
