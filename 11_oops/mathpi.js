359559f804c8056c42cfe83cf3836674e56271c0
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-01 11:23:42 +0000

    getOwnPropertyDescriptor

--- 11_oops/mathpi.js
+++ 11_oops/mathpi.js
@@ -1,2 +1,4 @@
 const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
 console.log(descripter);
+
+// getOwnPropertyDescriptor: this function returns the property descriptor of the specified property of the given object.
