da711ed44b3b6a025e25e5a426322188f3e49870
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-02 07:13:03 +0000

    print new properties

--- 11_oops/mathpi.js
+++ 11_oops/mathpi.js
@@ -41,3 +41,11 @@ Object.defineProperty(chai, "name", {
 	writable: false,
 	enumerable: false,
 });
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+// {
+//     value: 'Ginger',
+//     writable: false,
+//     enumerable: false,
+//     configurable: true
+// }
