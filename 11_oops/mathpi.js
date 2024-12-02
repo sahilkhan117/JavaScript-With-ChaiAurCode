99e05db65c21b1566764f52f4e58c05f56c5b74d
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-02 07:13:41 +0000

    with a condition

--- 11_oops/mathpi.js
+++ 11_oops/mathpi.js
@@ -51,4 +51,7 @@ console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 // }
 
 for (const [key, value] of Object.entries(chai)) {
+	if (typeof value !== "function") {
+        console.log(`${key} : ${value}`);
+    }
 }
