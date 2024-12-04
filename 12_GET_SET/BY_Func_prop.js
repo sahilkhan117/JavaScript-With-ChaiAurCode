f8b99352e2cf0485f492e40a7808d86bafc11e6e
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-04 14:21:41 +0000

    make set for email

--- 12_GET_SET/BY_Func_prop.js
+++ 12_GET_SET/BY_Func_prop.js
@@ -4,6 +4,7 @@ function User(email, password) {
 
     Object.defineProperty(this, "email", {
         get: function(){return this._email.toUpperCase()},
+        set: function(val){this._email = val}
     })
 
 }
