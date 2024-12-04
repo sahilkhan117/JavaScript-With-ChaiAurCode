4f048c8726578f0d3121e540c279975a83d1f07d
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-04 14:21:31 +0000

    make get for email

--- 12_GET_SET/BY_Func_prop.js
+++ 12_GET_SET/BY_Func_prop.js
@@ -2,6 +2,10 @@ function User(email, password) {
     this._email = email;
 	this._password = password;
 
+    Object.defineProperty(this, "email", {
+        get: function(){return this._email.toUpperCase()},
+    })
+
 }
 
 const chai = new User("sahil@ma.com", "asd")
