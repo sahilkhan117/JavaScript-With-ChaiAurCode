424487fafe252bc89d9ea21aac58d639f6dd2f71
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-06 18:21:20 +0000

    func prop done

--- 12_GET_SET/BY_Func_prop.js
+++ 12_GET_SET/BY_Func_prop.js
@@ -1,17 +1,25 @@
 function User(email, password) {
-    this._email = email;
+	this._email = email;
 	this._password = password;
 
-    Object.defineProperty(this, "email", {
-        get: function(){return this._email.toUpperCase()},
-        set: function(val){this._email = val}
-    })
+	Object.defineProperty(this, "email", {
+		get: function () {
+			return this._email.toUpperCase();
+		},
+		set: function (val) {
+			this._email = val;
+		},
+	});
 
-    Object.defineProperty(this, "password", {
-        get: function(){return this._password.toUpperCase()},
-        set: function(val){this._password = val}
-    })
+	Object.defineProperty(this, "password", {
+		get: function () {
+			return this._password.toUpperCase();
+		},
+		set: function (val) {
+			this._password = val;
+		},
+	});
 }
 
-const chai = new User("sahil@ma.com", "asd")
+const chai = new User("sahil@ma.com", "asd");
 console.log(chai.email);
