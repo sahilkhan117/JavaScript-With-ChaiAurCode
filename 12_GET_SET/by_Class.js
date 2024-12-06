cb2cec5dfe923346ea31574e071e5e71ec603534
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-06 18:22:15 +0000

    make user class

--- 12_GET_SET/by_Class.js
+++ 12_GET_SET/by_Class.js
@@ -1,3 +1,9 @@
 // 🆕🆕🆕
 // In ES6 2022 js Entroduce # to secure any property by default.
 
+class User {
+    constructor(email, password) {
+		this.email = email;
+		this.password = password;
+	}
+}
