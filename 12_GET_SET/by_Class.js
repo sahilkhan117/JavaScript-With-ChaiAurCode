f41998c81827308f79ca3f898c85d6b06627d45a
Sahil Khan <138899394+sahilkhan117@users.noreply.github.com>
2024-12-10 11:11:06 +0000

    make email get method

--- 12_GET_SET/by_Class.js
+++ 12_GET_SET/by_Class.js
@@ -3,7 +3,11 @@
 
 class User {
     constructor(email, password) {
-		this.email = email;
-		this.password = password;
+		this._email = email;
+		this._password = password;
+	}
+	
+	get email() {
+		return this._email.toUpperCase();
 	}
 }
